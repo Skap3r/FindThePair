0e8a5b9aacd069ae031f0b6e4a5473ebc0a64047
SkaperOmega <atifk@Gladiator>
2017-05-26 22:55:10 +0530

    Used mouseReleased instead of mouseClicked, which was not registering if clicked in quick succession.

--- src/findthepair/UI.java
+++ src/findthepair/UI.java
@@ -6,6 +6,7 @@
 package findthepair;
 
 import findthepair.game.Game;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 /**
@@ -58,165 +59,196 @@ public class UI extends javax.swing.JFrame {
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel1.setBackground(new java.awt.Color(36, 72, 103));
-        jPanel1.setLayout(new java.awt.GridLayout(4, 4));
+        jPanel1.setLayout(new java.awt.GridLayout(4, 4, 3, 3));
 
+        label1.setBackground(new java.awt.Color(36, 72, 103));
         label1.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label1.setOpaque(true);
         label1.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label1MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label1);
 
+        label2.setBackground(new java.awt.Color(36, 72, 103));
         label2.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label2.setOpaque(true);
         label2.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label2MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label2);
 
+        label3.setBackground(new java.awt.Color(36, 72, 103));
         label3.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label3.setOpaque(true);
         label3.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label3MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label3);
 
+        label4.setBackground(new java.awt.Color(36, 72, 103));
         label4.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label4.setOpaque(true);
         label4.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label4MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label4);
 
+        label5.setBackground(new java.awt.Color(36, 72, 103));
         label5.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label5.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label5.setOpaque(true);
         label5.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label5MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label5);
 
+        label6.setBackground(new java.awt.Color(36, 72, 103));
         label6.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label6.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label6.setOpaque(true);
         label6.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label6MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label6);
 
+        label7.setBackground(new java.awt.Color(36, 72, 103));
         label7.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label7.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label7.setOpaque(true);
         label7.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label7MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label7);
 
+        label8.setBackground(new java.awt.Color(36, 72, 103));
         label8.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label8.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label8.setOpaque(true);
         label8.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label8MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label8);
 
+        label9.setBackground(new java.awt.Color(36, 72, 103));
         label9.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label9.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label9.setOpaque(true);
         label9.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label9MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label9);
 
+        label10.setBackground(new java.awt.Color(36, 72, 103));
         label10.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label10.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label10.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label10.setOpaque(true);
         label10.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label10MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label10);
 
+        label11.setBackground(new java.awt.Color(36, 72, 103));
         label11.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label11.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label11.setOpaque(true);
         label11.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label11MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label11);
 
+        label12.setBackground(new java.awt.Color(36, 72, 103));
         label12.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label12.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label12.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label12.setOpaque(true);
         label12.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label12MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label12);
 
+        label13.setBackground(new java.awt.Color(36, 72, 103));
         label13.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label13.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label13.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label13.setOpaque(true);
         label13.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label13MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label13);
 
+        label14.setBackground(new java.awt.Color(36, 72, 103));
         label14.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label14.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label14.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label14.setOpaque(true);
         label14.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label14MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label14);
 
+        label15.setBackground(new java.awt.Color(36, 72, 103));
         label15.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label15.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label15.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label15.setOpaque(true);
         label15.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label15MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label15);
 
+        label16.setBackground(new java.awt.Color(36, 72, 103));
         label16.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         label16.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         label16.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        label16.setOpaque(true);
         label16.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                label16MouseClicked(evt);
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                label1MouseReleased(evt);
             }
         });
         jPanel1.add(label16);
@@ -226,80 +258,62 @@ public class UI extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void label1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label1MouseClicked
-        sendInfo(label1, (short)1);
-    }//GEN-LAST:event_label1MouseClicked
-
-    private void label2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label2MouseClicked
-        sendInfo(label2, (short)2);
-    }//GEN-LAST:event_label2MouseClicked
-
-    private void label3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label3MouseClicked
-        sendInfo(label3, (short)3);
-    }//GEN-LAST:event_label3MouseClicked
-
-    private void label4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label4MouseClicked
-        sendInfo(label4, (short)4);
-    }//GEN-LAST:event_label4MouseClicked
-
-    private void label5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label5MouseClicked
-        sendInfo(label5, (short)5);
-    }//GEN-LAST:event_label5MouseClicked
-
-    private void label6MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label6MouseClicked
-        sendInfo(label6, (short)6);
-    }//GEN-LAST:event_label6MouseClicked
-
-    private void label7MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label7MouseClicked
-        sendInfo(label7, (short)7);
-    }//GEN-LAST:event_label7MouseClicked
-
-    private void label8MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label8MouseClicked
-        sendInfo(label8, (short)8);
-    }//GEN-LAST:event_label8MouseClicked
-
-    private void label9MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label9MouseClicked
-        sendInfo(label9, (short)9);
-    }//GEN-LAST:event_label9MouseClicked
-
-    private void label10MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label10MouseClicked
-        sendInfo(label10, (short)10);
-    }//GEN-LAST:event_label10MouseClicked
-
-    private void label11MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label11MouseClicked
-        sendInfo(label11, (short)11);
-    }//GEN-LAST:event_label11MouseClicked
-
-    private void label12MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label12MouseClicked
-        sendInfo(label12, (short)12);
-    }//GEN-LAST:event_label12MouseClicked
-
-    private void label13MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label13MouseClicked
-        sendInfo(label13, (short)13);
-    }//GEN-LAST:event_label13MouseClicked
-
-    private void label14MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label14MouseClicked
-        sendInfo(label14, (short)14);
-    }//GEN-LAST:event_label14MouseClicked
-
-    private void label15MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label15MouseClicked
-        sendInfo(label15, (short)15);
-    }//GEN-LAST:event_label15MouseClicked
-
-    private void label16MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label16MouseClicked
-        sendInfo(label16, (short)16);
-    }//GEN-LAST:event_label16MouseClicked
+    private void label1MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_label1MouseReleased
+        JLabel temp = new JLabel();
+        
+        temp = (JLabel) evt.getSource();
+        
+        if (temp == label1)
+            sendInfo(label1, (short)1);
+        else if (temp == label2)
+            sendInfo(label2, (short)2);
+        else if (temp == label3)
+            sendInfo(label3, (short)3);
+        else if (temp == label4)
+            sendInfo(label4, (short)4);
+        else if (temp == label5)
+            sendInfo(label5, (short)5);
+        else if (temp == label6)
+            sendInfo(label6, (short)6);
+        else if (temp == label7)
+            sendInfo(label7, (short)7);
+        else if (temp == label8)
+            sendInfo(label8, (short)8);
+        else if (temp == label9)
+            sendInfo(label9, (short)9);
+        else if (temp == label10)
+            sendInfo(label10, (short)10);
+        else if (temp == label11)
+            sendInfo(label11, (short)11);
+        else if (temp == label12)
+            sendInfo(label12, (short)12);
+        else if (temp == label13)
+            sendInfo(label13, (short)13);
+        else if (temp == label14)
+            sendInfo(label14, (short)14);
+        else if (temp == label15)
+            sendInfo(label15, (short)15);
+        else if (temp == label16)
+            sendInfo(label16, (short)16);
+    }//GEN-LAST:event_label1MouseReleased
     
     private void sendInfo(javax.swing.JLabel label, short position){
         Game game = new Game();
         
         counter++;
-        System.out.println(counter);
         
         if (game.checkIfAllDisabled()) {
             counter--;
-            String s = "You took " +counter +" moves to complete";
-            JOptionPane.showMessageDialog(rootPane, s);
+            if (counter == 16) {
+                String s = "Perfect Score!!! You completed in the minimum "
+                        + "number of moves available.";
+                JOptionPane.showMessageDialog(rootPane, s, "Result", 
+                        JOptionPane.PLAIN_MESSAGE);
+            } else if (counter > 16) {
+                String s = "You took " +counter +" moves to complete.";
+                JOptionPane.showMessageDialog(rootPane, s, "Result", 
+                        JOptionPane.PLAIN_MESSAGE);
+            }
         }
             
         while (this.once) {
